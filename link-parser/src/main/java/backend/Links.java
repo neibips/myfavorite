d2db010a9a0a195c0389588a9f2cf95c508a1e68
neibips <68771174+neibips@users.noreply.github.com>
2023-04-05 00:20:51 +0500

    исправление ошибок в работе linkparser

--- link-parser/src/main/java/backend/Links.java
+++ link-parser/src/main/java/backend/Links.java
@@ -1,7 +1,14 @@
 package backend;
 
+import reactor.core.publisher.Mono;
+
+import java.net.URISyntaxException;
+
 public sealed interface Links permits GitHubURL, StackOverflowURL {
-    public default String getData(String URL){
+    public default String getData(String URL) throws URISyntaxException {
         return "null";
+    }
+    default Mono<String> getAllLinks(String URL) throws URISyntaxException {
+        return null;
     }
 }
